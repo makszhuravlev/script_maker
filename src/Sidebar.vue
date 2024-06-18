0d44ce8d06f3eadc667aaf5606418bcaac30a15d
Alexandr <s.stepantzover@yandex.ru>
2024-06-18 20:50:36 +0300

    Fix Text Slidebar

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -8,7 +8,7 @@ const { onDragStart } = useDragAndDrop()
 
 <template>
   <aside>
-    <div class="description">You can drag these nodes to the pane.</div>
+    <div class="description">Элементы:</div>
     
     <div class="nodes">
       <div class="vue-flow__node-default" :draggable="true" @dragstart="onDragStart($event, 'default')">Фраза клиента</div>
